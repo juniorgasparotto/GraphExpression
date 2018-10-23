7ef776377f6a45e5c092c8f41a4bc14653eabfbb
Glauber Donizeti Gasparotto Junior <juniorgasparotto@msn.com>
2018-10-22 22:02:50 -0300

    doc

---
+++ src/GraphExpression.Examples/Examples/SearchGraphInfo.cs
@@ -0,0 +1,33 @@
+﻿using GraphExpression.Examples.Models;
+using SysCommand.ConsoleApp;
+using SysCommand.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphExpression.Examples
+{
+    public partial class Examples
+    {
+        [Action(Help = "Remove graph coexistents")]
+        public void GraphInfo()
+        {
+            var A = new CircularEntity("A");
+            var B = new CircularEntity("B");
+            var C = new CircularEntity("C");
+            var D = new CircularEntity("D");
+
+            A = A + B + (C + D);
+            var expressionA = A.AsExpression(f => f.Children, e => e.Name);
+
+            foreach (Edge<CircularEntity> edge in expressionA.Graph.Edges)
+                System.Console.WriteLine(edge.ToString());
+
+            foreach (Path<CircularEntity> path in expressionA.Graph.Paths)
+                System.Console.WriteLine(path.ToString());
+
+            foreach (EntityItem<CircularEntity> item in expressionA)
+                System.Console.WriteLine($"{item.ToString()} => {item.Path}");
+        }
+    }
+}
---
+++ src/GraphExpression.Examples/Examples/SearchGraphRemoveCoexistents.cs
@@ -0,0 +1,43 @@
+﻿using GraphExpression.Examples.Models;
+using SysCommand.ConsoleApp;
+using SysCommand.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphExpression.Examples
+{
+    public partial class Examples
+    {
+        [Action(Help = "Remove graph coexistents")]
+        public void GraphRemoveCoexistents()
+        {
+            var A = new CircularEntity("A");
+            var B = new CircularEntity("B");
+            var C = new CircularEntity("C");
+            var D = new CircularEntity("D");
+
+            A = A + B + (C + D);
+
+            var graphs = new List<Graph<CircularEntity>>
+            {
+                A.AsExpression(f=>f.Children, e => e.Name).Graph,
+                C.AsExpression(f=>f.Children, e => e.Name).Graph
+            };
+
+            System.Console.WriteLine($"-> A: HashCode: {graphs[0].GetHashCode()}");
+            foreach (Path<CircularEntity> path in graphs[0].Paths)
+                System.Console.WriteLine(path.ToString());
+
+            System.Console.WriteLine($"-> B: HashCode: {graphs[1].GetHashCode()}");
+            foreach (Path<CircularEntity> path in graphs[1].Paths)
+                System.Console.WriteLine(path.ToString());
+
+            var graphsNonDuplicates = graphs.RemoveCoexistents();
+            foreach(var graph in graphsNonDuplicates)
+            {
+                System.Console.WriteLine($"-> HashCode not duplicates: {graph.GetHashCode()}");
+            }
+        }
+    }
+}
